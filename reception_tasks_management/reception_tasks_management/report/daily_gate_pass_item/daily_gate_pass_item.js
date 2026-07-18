11f4b2b8fb1826477d758dd382d5d9915fa37789
Harshit Jain <jnharshit1998@gmail.com>
2026-07-18 09:21:31 +0000

    Removed Columns from Report

--- reception_tasks_management/reception_tasks_management/report/daily_gate_pass_item/daily_gate_pass_item.js
+++ reception_tasks_management/reception_tasks_management/report/daily_gate_pass_item/daily_gate_pass_item.js
@@ -1,8 +1,15 @@
 frappe.query_reports["Daily Gate Pass Item"] = {
-        filters: [
+    filters: [
         {
-            fieldname: "date",
-            label: __("Date"),
+            fieldname: "from_date",
+            label: __("From Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_days(frappe.datetime.get_today(), -6),
+            reqd: 1
+        },
+        {
+            fieldname: "to_date",
+            label: __("To Date"),
             fieldtype: "Date",
             default: frappe.datetime.get_today(),
             reqd: 1
